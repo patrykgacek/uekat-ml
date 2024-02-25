549cd51e0566f3495ff2a7a9a0fbf196c1120d7b
Patryk Gacek <haniv1001@gmail.com>
2024-02-25 22:13:09 +0100

    feat: Load data, count occurrences

---
+++ main.py
@@ -0,0 +1,35 @@
+import csv
+import os
+import numpy as np
+
+def load_data(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data
+
+
+def count_occur(data):
+    hist = []
+    
+    # Init list with attributes names from first row
+    for attr in data[0]:
+        hist.append({attr: 0})
+
+    # Count occurances of each attribute
+    for row in data:
+        for idx_attr, attr in enumerate(row):
+            if attr in hist[idx_attr]:
+                hist[idx_attr][attr] = hist[idx_attr][attr] + 1
+            else:
+                hist[idx_attr][attr] = 1
+
+    return hist
+
+
+
+files = os.listdir('data')
+for file in files:
+    data = load_data('data/' + file)
+    histogram = count_occur(data)
+    print(file, histogram)
